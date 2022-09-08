15b93f0e6e0a4a1a6fbfbdb905add9241f2b3cb1
Dmytro Svirsa <104418771+DmytroSvirsa@users.noreply.github.com>
2022-09-08 15:07:37 +0300

    Update views.py

--- taxi/views.py
+++ taxi/views.py
@@ -34,7 +34,7 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
     template_name = "taxi/manufacturer_list.html"
-    paginate_by = 2
+    paginate_by = 5
 
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
@@ -56,7 +56,7 @@ class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
-    paginate_by = 2
+    paginate_by = 5
     queryset = Car.objects.all().select_related("manufacturer")
 
 
@@ -83,7 +83,7 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
-    paginate_by = 2
+    paginate_by = 5
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
