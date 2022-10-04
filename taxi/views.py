f13491bf1dd2aab341f6989df0c9322d4fd6ca4c
KevinXD <kevinfdevid@gmail.com>
2022-10-04 19:48:05 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -6,8 +6,12 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm, CarSearchForm, ManufacturerSearchForm, \
-    DriverSearchForm
+from .forms import (DriverCreationForm,
+                    DriverLicenseUpdateForm,
+                    CarForm,
+                    CarSearchForm,
+                    ManufacturerSearchForm,
+                    DriverSearchForm)
 
 
 @login_required
