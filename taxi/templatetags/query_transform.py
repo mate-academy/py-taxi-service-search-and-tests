59eca15fdbe47ac5273df54066aab186dd43c5a5
Andriy Demeshko <ande777complaint@gmail.com>
2022-10-22 16:15:13 +0300

    'Solution_edited'

--- taxi/templatetags/query_transform.py
+++ taxi/templatetags/query_transform.py
@@ -6,9 +6,9 @@ register = template.Library()
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
-    for key, val in kwargs.items():
-        if val is not None:
-            updated[key] = val
+    for key, val_ in kwargs.items():
+        if val_ is not None:
+            updated[key] = val_
         else:
             updated.pop(key, 0)
     return updated.urlencode()
