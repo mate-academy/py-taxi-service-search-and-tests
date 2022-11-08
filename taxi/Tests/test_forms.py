59e05352eaf57b4e7f36632b90ce5318cf714208
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-11-08 22:45:38 +0200

    Solution

--- taxi/Tests/test_forms.py
+++ taxi/Tests/test_forms.py
@@ -26,3 +26,26 @@ class FormsTests(TestCase):
 
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
+
+
+class ValidLicenseNumberFormTests(TestCase):
+    @staticmethod
+    def create_form(test_license_number):
+        return DriverLicenseUpdateForm(
+            data={"license_number": test_license_number}
+        )
+
+    def test_validation_license_number_with_valid_data(self):
+        self.assertTrue(self.create_form("AAA11111").is_valid())
+
+    def test_length_of_license_number_should_be_not_more_than_8(self):
+        self.assertFalse(self.create_form("AAA111116").is_valid())
+
+    def test_length_of_license_number_should_be_not_less_than_8(self):
+        self.assertFalse(self.create_form("AAA1114").is_valid())
+
+    def test_first_3_characters_should_be_uppercase_letters(self):
+        self.assertFalse(self.create_form("aAA11111").is_valid())
+
+    def test_last_5_characters_should_be_be_digits(self):
+        self.assertFalse(self.create_form("AAAA1111").is_valid())
