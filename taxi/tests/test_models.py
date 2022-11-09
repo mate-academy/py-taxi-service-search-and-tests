1f9ed183e977d41ca06d113b67e7b4d44f0ec871
HStakhiv <gsstahiv@gmail.com>
2022-11-09 21:55:38 +0200

    Fix flake8

--- taxi/tests/test_models.py
+++ taxi/tests/test_models.py
@@ -6,7 +6,9 @@ from taxi.models import Manufacturer, Car
 
 class ModelsTests(TestCase):
     def setUp(self) -> None:
-        self.manufacturer = Manufacturer.objects.create(name="test", country="test")
+        self.manufacturer = Manufacturer.objects.create(
+            name="test", country="test"
+        )
 
         self.username = "test.test"
         self.password = "test12345"
@@ -26,13 +28,15 @@ class ModelsTests(TestCase):
 
     def test_manufacturer_str(self):
         self.assertEqual(
-            str(self.manufacturer), f"{self.manufacturer.name} {self.manufacturer.country}"
+            str(self.manufacturer),
+            f"{self.manufacturer.name} {self.manufacturer.country}",
         )
 
     def test_driver_str(self):
         self.assertEqual(
             str(self.driver),
-            f"{self.driver.username} ({self.driver.first_name} {self.driver.last_name})",
+            f"{self.driver.username} "
+            f"({self.driver.first_name} {self.driver.last_name})",
         )
 
     def test_car_str(self):
