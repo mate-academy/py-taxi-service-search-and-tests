fabdb733c7bbd9def4a72a0b4cc708f7a5693b5f
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-09 16:30:39 +0200

    improvement for validation test

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -20,14 +20,18 @@ class FormsTest(TestCase):
 
     def test_driver_creation_form_with_invalid_licence_num(self):
         """Test whether licence number validation works"""
-        form_data = {
-            "username": "testuser",
-            "password1": "Testpass123",
-            "password2": "Testpass123",
-            "first_name": "jack test",
-            "last_name": "black test",
-            "license_number": "JAk12312",
-        }
-        form = DriverCreationForm(data=form_data)
-        self.assertFalse(form.is_valid())
-        self.assertNotEqual(form.cleaned_data, form_data)
+
+        bad_license_number = ["JAk12312", "1111111", "jac"]
+        for license_number in bad_license_number:
+            with self.subTest(license_number=license_number):
+                form_data = {
+                    "username": "testuser",
+                    "password1": "Testpass123",
+                    "password2": "Testpass123",
+                    "first_name": "jack test",
+                    "last_name": "black test",
+                    "license_number": license_number,
+                }
+                form = DriverCreationForm(data=form_data)
+                self.assertFalse(form.is_valid())
+                self.assertNotEqual(form.cleaned_data, form_data)
