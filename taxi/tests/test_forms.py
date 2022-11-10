374b68d60a4a4ef2c13ad4134e424bb7c86c3800
Viktor Beniukh <superbigbenjamin@gmail.com>
2022-11-10 19:26:59 +0200

    'MySolution'

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 
-from taxi.forms import DriverCreationForm
+from taxi.forms import (
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+    DriverSearchForm,
+    CarSearchForm,
+    ManufacturerSearchForm
+)
 
 
 class FormTests(TestCase):
@@ -15,6 +21,41 @@ class FormTests(TestCase):
         }
 
         form = DriverCreationForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+    def test_update_license_number(self):
+        form = DriverLicenseUpdateForm({"license_number": "LCN13579"})
+
+        self.assertTrue(form.is_valid())
+
+    def test_correct_entry_license_number(self):
+        incorrect_license_number = [
+            "AWE1234", "Dfx45678", "ADV56I85", "KL189756"
+        ]
+        for license_number in incorrect_license_number:
+            form = DriverLicenseUpdateForm({"license_number": license_number})
+
+            self.assertFalse(form.is_valid())
+
+    def test_driver_search_form(self):
+        form_data = {"username": "test.user"}
+        form = DriverSearchForm(form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+    def test_car_search_form(self):
+        form_data = {"model": "test model"}
+        form = CarSearchForm(form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+    def test_manufacturer_search_form(self):
+        form_data = {"name": "test name"}
+        form = ManufacturerSearchForm(form_data)
 
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
