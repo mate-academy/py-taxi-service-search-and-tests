1feefa6e993d2203285737c75c7aca68ae3a857c
Viktor Beniukh <superbigbenjamin@gmail.com>
2022-11-10 10:51:13 +0200

    'MySolution'

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from taxi.forms import DriverCreationForm
+
+
+class FormTests(TestCase):
+    def test_driver_creation_form_with_license_first_last_name_is_valid(self):
+        form_data = {
+            "username": "new_user",
+            "password1": "user123test",
+            "password2": "user123test",
+            "first_name": "Test first",
+            "last_name": "Test last",
+            "license_number": "NWU12345"
+        }
+
+        form = DriverCreationForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
