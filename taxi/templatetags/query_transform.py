6af2453065bd6d21872f15c5863ef04c4b9b03ac
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-11-12 01:32:24 +0200

    Changed html formatting

--- taxi/templatetags/query_transform.py
+++ taxi/templatetags/query_transform.py
@@ -5,8 +5,6 @@ register = template.Library()
 
 @register.simple_tag
 def query_transform(request, **kwargs):
-    print("request.GET", request.GET)
-    print("kwargs", kwargs)
     updated = request.GET.copy()
     for key, value in kwargs.items():
         if value is not None:
