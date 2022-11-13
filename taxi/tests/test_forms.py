4582159f6249d8c96ca5ee9dbf84593f57916965
Vasyl Poremchuk <nleveryone@gmail.com>
2022-11-13 11:11:48 +0200

    Fix test and add new test to the test_forms.py file

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -2,18 +2,18 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.forms import DriverCreationForm
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
 
 
 class FormsTests(TestCase):
     def setUp(self) -> None:
         self.form_data = {
             "username": "jim.hopper",
-            "password1": "bobo765",
-            "license_number": "JIM26531",
-            "password2": "bobo765",
+            "password1": "bobo7657",
+            "password2": "bobo7657",
             "first_name": "Jim",
             "last_name": "Hopper",
+            "license_number": "JIM26531",
         }
 
     def test_driver_creation_(self):
@@ -21,3 +21,15 @@ class FormsTests(TestCase):
 
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, self.form_data)
+
+    def test_driver_license_update_form(self):
+        self.form_data = {
+            "license_number": "UTY89424",
+        }
+        form = DriverLicenseUpdateForm(
+            data=self.form_data,
+        )
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data, self.form_data,
+        )
