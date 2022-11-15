3469c0ef4b576e5845f8d5a79c15e864c2cad353
Kirill Drago <dragoswim7@gmail.com>
2022-11-15 13:30:45 +0200

    ixed solution

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -26,3 +26,26 @@ class FormsTests(TestCase):
 
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
+
+
+class ValidLicenseNumberFormTests(TestCase):
+    @staticmethod
+    def create_form(test_license_number):
+        return DriverLicenseUpdateForm(
+            data={"license_number": test_license_number}
+        )
+
+    def test_validation_license_number_with_valid_data(self):
+        self.assertTrue(self.create_form("TES12345").is_valid())
+
+    def test_length_of_license_number_should_be_not_more_than_8(self):
+        self.assertFalse(self.create_form("TES123456").is_valid())
+
+    def test_length_of_license_number_should_be_not_less_than_8(self):
+        self.assertFalse(self.create_form("TES1234").is_valid())
+
+    def test_first_3_characters_should_be_uppercase_letters(self):
+        self.assertFalse(self.create_form("TE123456").is_valid())
+
+    def test_last_5_characters_should_be_be_digits(self):
+        self.assertFalse(self.create_form("TEST2345").is_valid())
--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -76,7 +76,7 @@ class PrivateManufacturerTests(TestCase):
         self.assertTemplateUsed(res, "taxi/manufacturer_list.html")
 
     def test_manufacturer_search(self):
-        response = self.client.get("/manufacturers/?name=Ford")
+        response = self.client.get(MANUFACTURER_LIST_URL + "?name=Ford")
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(
             response.context["manufacturer_list"],
@@ -140,29 +140,6 @@ class PrivateDriverTests(TestCase):
         user = get_user_model().objects.get(username=form_data["username"])
 
         self.assertEqual(user.license_number, form_data["license_number"])
-
-
-class ValidLicenseNumberFormTests(TestCase):
-    @staticmethod
-    def create_form(test_license_number):
-        return DriverLicenseUpdateForm(
-            data={"license_number": test_license_number}
-        )
-
-    def test_validation_license_number_with_valid_data(self):
-        self.assertTrue(self.create_form("TES12345").is_valid())
-
-    def test_length_of_license_number_should_be_not_more_than_8(self):
-        self.assertFalse(self.create_form("TES123456").is_valid())
-
-    def test_length_of_license_number_should_be_not_less_than_8(self):
-        self.assertFalse(self.create_form("TES1234").is_valid())
-
-    def test_first_3_characters_should_be_uppercase_letters(self):
-        self.assertFalse(self.create_form("TE123456").is_valid())
-
-    def test_last_5_characters_should_be_be_digits(self):
-        self.assertFalse(self.create_form("TEST2345").is_valid())
 
 
 class CarTest(TestCase):
