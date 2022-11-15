1b7b7e8ec633aac35e664d807c63e1089412b501
Iurii Lobunets <iurii@lobunets.com.ua>
2022-11-15 22:01:21 +0200

    Solution of py-taxi-service-search-and-tests

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+from taxi.forms import DriverCreationForm
+
+
+class FormsTests(TestCase):
+    def test_driver_creation_form_with_main_fields_is_valid(self):
+        """Test that driver's main fields correctly checked on form"""
+        form_data = {
+            "username": "test",
+            "password1": "test2345",
+            "password2": "test2345",
+            "first_name": "Test First Name",
+            "last_name": "Test Last Name",
+            "license_number": "AAA12345",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+    def test_driver_creation_form_with_license_number_is_no_valid(self):
+        """Test that driver's license_number checked on form"""
+        form_data = {
+            "username": "test",
+            "password1": "test2345",
+            "password2": "test2345",
+            "license_number": "AaA12a456",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
