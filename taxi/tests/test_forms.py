3f256fe378d868905daa38d152b1e883bf970643
Taras Andruschenko <And999110@gmail.com>
2022-11-16 16:16:53 +0200

    Add SearchForm tests

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,18 +1,37 @@
+from django.contrib.auth import get_user_model
+
 from django.test import TestCase
 
-from taxi.forms import DriverCreationForm
+from taxi.models import Car, Manufacturer, Driver
 
 
-class FormsTests(TestCase):
-    def test_driver_creation_form_with_additional_fields(self):
-        form_data = {
-            "username": "test user",
-            "license_number": "test number",
-            "first_name": "test name",
-            "last_name": "test last name",
-            "password1": "test password",
-            "password2": "test password",
-        }
-        form = DriverCreationForm(data=form_data)
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data, form_data)
+class SearchFormTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            "user", "user12345"
+        )
+        self.client.force_login(self.user)
+
+    def test_car_search(self):
+        response = self.client.get("/cars/?model=mercedes")
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context["car_list"],
+            Car.objects.filter(model__icontains="mercedes"),
+        )
+
+    def test_driver_serch(self):
+        response = self.client.get("/drivers/?username=Johnny")
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context["driver_list"],
+            Driver.objects.filter(username__icontains="Johnny")
+        )
+
+    def test_manufacturer_search(self):
+        response = self.client.get("/manufacturers/?name=Mitsu")
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context["manufacturer_list"],
+            Manufacturer.objects.filter(name__icontains="Mitsu")
+        )
