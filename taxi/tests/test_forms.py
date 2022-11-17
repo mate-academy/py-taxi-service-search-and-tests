2d91c6ae9be2877447e9d9a85e71789af5460f6f
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-17 02:35:36 +0200

    Add query_transform file

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from taxi.forms import DriverCreationForm
+from taxi.forms import DriverCreationForm, DriverSearchForm, CarSearchForm
+from taxi.models import Driver, Car, Manufacturer
 
 
 class FormsTests(TestCase):
@@ -16,3 +17,32 @@ class FormsTests(TestCase):
         form = DriverCreationForm(data=data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, data)
+
+    def test_driver_search_form_valid_data(self):
+        driver = Driver.objects.create(username="test", license_number="FRE12345")
+        data = {
+            "driver_0": driver.username
+        }
+        form = DriverSearchForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_car_search_form_valid_data(self):
+        manufacturer = Manufacturer.objects.create(name="test")
+        driver = Driver.objects.create(username="test", license_number="FRE12345")
+        car = Car.objects.create(
+            model="test",
+            manufacturer=manufacturer,
+        )
+        data = {
+            "car_0": car.model
+        }
+        form = CarSearchForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_driver_search_form_invalid_data(self):
+        data = {
+            "driver_0": "di",
+            "driver_1": ""
+        }
+        form = DriverSearchForm(data=data)
+        self.assertFalse(form.is_valid())
