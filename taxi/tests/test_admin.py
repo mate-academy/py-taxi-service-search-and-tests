737a1153c56fe2396a4de198681208e2adfe429d
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-18 19:24:45 +0200

    Rename res on response

--- taxi/tests/test_admin.py
+++ taxi/tests/test_admin.py
@@ -16,18 +16,18 @@ class AdminSiteTests(TestCase):
 
     def test_driver_license_number_listed(self):
         url = reverse("admin:taxi_driver_changelist")
-        res = self.client.get(url)
+        response = self.client.get(url)
 
-        self.assertContains(res, self.driver.license_number)
+        self.assertContains(response, self.driver.license_number)
 
     def test_driver_detailed_license_number_listed(self):
         url = reverse("admin:taxi_driver_change", args=[self.driver.id])
-        res = self.client.get(url)
+        response = self.client.get(url)
 
-        self.assertContains(res, self.driver.license_number)
+        self.assertContains(response, self.driver.license_number)
 
     def test_driver_add_fieldsets(self):
         url = reverse("admin:taxi_driver_add")
-        res = self.client.get(url)
+        response = self.client.get(url)
 
-        self.assertContains(res, "license_number")
+        self.assertContains(response, "license_number")
