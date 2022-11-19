6ed951a566f7552fff33d60e39d241f51061e795
Davyd Kryvko <davidkrivko@gmail.com>
2022-11-19 14:13:21 +0100

    Solution_4

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -5,40 +5,44 @@ from taxi.forms import DriverCreationForm
 
 class FormsTests(TestCase):
     def test_driver_creation_form_with_new_params_is_valid(self):
-        form_data = [{
+        form_data = {
             "username": "user123",
             "password1": "test12345",
             "password2": "test12345",
             "first_name": "volodya",
             "last_name": "krivko",
             "license_number": "AAA12345",
-        },
-            {"username": "user123",
-             "password1": "test12344",
-             "password2": "test12345",
-             "first_name": "volodya",
-             "last_name": "krivko",
-             "license_number": "AAA12345",
-             },
-            {"username": "user123",
-             "password1": "test12345",
-             "password2": "test12345",
-             "first_name": "volodya",
-             "last_name": "krivko",
-             "license_number": "AA12345",
-             }]
+        }
 
-        form = DriverCreationForm(data=form_data[0])
+        form_data_1 = {
+            "username": "user123",
+            "password1": "test12345",
+            "password2": "test12345",
+            "first_name": "volodya",
+            "last_name": "krivko",
+            "license_number": "AA12345",
+        }
+
+        form_data_2 = {
+            "username": "user123",
+            "password1": "test12344",
+            "password2": "test12345",
+            "first_name": "volodya",
+            "last_name": "krivko",
+            "license_number": "AAA12345",
+        }
+
+        form = DriverCreationForm(data=form_data)
 
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
 
-        form = DriverCreationForm(data=form_data[1])
+        form = DriverCreationForm(data=form_data_1)
 
         self.assertFalse(form.is_valid())
         self.assertNotEqual(form.cleaned_data, form_data)
 
-        form = DriverCreationForm(data=form_data[2])
+        form = DriverCreationForm(data=form_data_2)
 
         self.assertFalse(form.is_valid())
         self.assertNotEqual(form.cleaned_data, form_data)
