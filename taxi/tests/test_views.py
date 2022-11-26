86e6c7234fdf84242a53c54a97d29623511b8d35
Nikyxa <kolisova.veronika81@gmail.com>
2022-11-26 17:16:53 +0200

    Add tests for search feature

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -109,3 +109,33 @@ class DriverCarTests(TestCase):
             list(drivers)
         )
         self.assertTemplateUsed(response, "taxi/driver_list.html")
+
+
+class SearchTests(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="sandychicks",
+            license_number="JKQ12345"
+        )
+        self.client.force_login(self.user)
+
+    def test_search_drivers_by_username(self):
+        response = self.client.get(reverse("taxi:driver-list") + "?name=sandychicks")
+        self.assertEqual(
+            list(response.context["driver_list"]),
+            list(Driver.objects.filter(username__icontains="sandychicks")),
+        )
+
+    def test_search_cars_by_model(self):
+        response = self.client.get(reverse("taxi:car-list") + "?name=Civic")
+        self.assertEqual(
+            list(response.context["car_list"]),
+            list(Car.objects.filter(model__icontains="Civic")),
+        )
+
+    def test_search_manufacturer_by_name(self):
+        response = self.client.get(reverse("taxi:manufacturer-list") + "?name=Honda")
+        self.assertEqual(
+            list(response.context["manufacturer_list"]),
+            list(Manufacturer.objects.filter(name__icontains="Honda")),
+        )
