760d60fae0e87f70d8febbcb88ed52ffb26116e7
eugene <bantyc8@gmail.com>
2022-11-28 20:56:14 +0200

    new solution

--- taxi/tests/test_admin.py
+++ taxi/tests/test_admin.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from taxi.models import Car
 
 
 class AdminSiteTest(TestCase):
