22e3e60396bcb35645bf41f6d5f44ee22316ddc8
Bohdan2001007 <bogdan1991007@gmail.com>
2022-12-28 19:37:28 +0100

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -50,3 +50,30 @@ def validate_license_number(
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+
+class CarSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by model"})
+    )
+
+
+class DriverSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by license number"})
+    )
+
+
+class ManufacturerSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
---
+++ taxi/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Car
+
+
+class ModelsTests(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(name="BMW",
+                                                   country="Germany")
+
+        self.assertEqual(
+            str(manufacturer),
+            f"{manufacturer.name} {manufacturer.country}",
+        )
+
+    def test_driver_str(self):
+        driver = get_user_model().objects.create(
+            username="test",
+            password="test12345",
+            first_name="Test first",
+            last_name="Test last",
+        )
+
+        self.assertEqual(str(driver), f"{driver.username} ({driver.first_name} {driver.last_name})")
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(name="BMW",
+                                                   country="Germany")
+        car = Car.objects.create(
+            model="Test",
+            manufacturer=manufacturer
+        )
+
+        self.assertEqual(str(car), f"{car.model}")
