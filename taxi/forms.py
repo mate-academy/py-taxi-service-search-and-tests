17d039cc9c7d7a689978ac1894300d41ed638c3d
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-15 17:10:56 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -50,3 +50,15 @@ def validate_license_number(
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+
+class DriversSearchForm(forms.Form):
+    username = forms.CharField(max_length=255, required=False)
+
+
+class CarSearchForm(forms.Form):
+    model = forms.CharField(max_length=255, required=False)
+
+
+class ManufacturerSearchForm(forms.Form):
+    name = forms.CharField(max_length=255, required=False)
