48accac4be20aca44bcf3802437f264eb377a348
Denys Tereshchuk <terrya@ukr.net>
2023-01-25 00:55:23 +0400

    Added test forms

---
+++ tests/test_forms/test_driverlicenseupdateform.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+from taxi.forms import DriverLicenseUpdateForm
+
+
+class DriverLicenseUpdateFormTests(TestCase):
+    @staticmethod
+    def create_form(test_number):
+        return DriverLicenseUpdateForm({"license_number": test_number})
+
+    def test_license_number_with_valid_data_is_valid(self):
+        self.assertTrue(self.create_form("QWE12345").is_valid())
+
+    def test_license_number_first_3_symbol_is_alphabet(self):
+        self.assertFalse(self.create_form("QW123456").is_valid())
+
+    def test_license_number_first_3_symbol_is_upper(self):
+        self.assertFalse(self.create_form("QwE12345").is_valid())
+
+    def test_license_number_last_5_symbol_is_digit(self):
+        self.assertFalse(self.create_form("QWER1234").is_valid())
+
+    def test_license_number_length_less_8_digits(self):
+        self.assertFalse(self.create_form("QWE12").is_valid())
+
+    def test_license_number_length_bigger_8_digits(self):
+        self.assertFalse(self.create_form("QWER123456").is_valid())
+
+
