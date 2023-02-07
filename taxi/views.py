aa7999d4e47514d6fc1251b451ba4e7207540b7c
Ivan Mayor <maramorosh2@gmail.com>
2023-02-07 19:34:28 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -46,7 +46,7 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ManufacturerListView, self).get_context_data(**kwargs)
 
-        name = self.request.GET.get("name")
+        name = self.request.GET.get("name", "")
 
         context["search_form"] = ManufacturerSearchForm(
             initial={
