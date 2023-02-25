43b6e46abf40d4127e70fba21b5229ac2476bcb4
Anna Kabatova <kabatova.anna@ukr.net>
2023-02-25 15:52:30 +0200

    initial

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,79 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.forms import DriverCreationForm, validate_license_number
+
+
+class FormsTests(TestCase):
+	def test_driver_creation_form_with_pseudonym_first_last_name_is_valid(self):
+		form_data = {
+			"username": "new_user",
+			"password1": "user123test",
+			"password2": "user123test",
+			"first_name": "Test first",
+			"last_name": "Test last",
+			"license_number": "TES12345"
+		}
+		form = DriverCreationForm(data=form_data)
+		self.assertTrue(form.is_valid())
+		self.assertEqual(form.cleaned_data, form_data)
+
+
+class PrivateDriverTests(TestCase):
+	def setUp(self) -> None:
+		self.user = get_user_model().objects.create_user(
+			"test",
+			"password123"
+		)
+		self.client.force_login(self.user)
+
+	def test_create_driver(self):
+		form_data = {
+			"username": "new_user",
+			"password1": "user123test",
+			"password2": "user123test",
+			"first_name": "Test first",
+			"last_name": "Test last",
+			"license_number": "TES12345"
+		}
+		self.client.post(reverse("taxi:driver-create"), data=form_data)
+		new_user = get_user_model().objects.get(username=form_data["username"])
+
+		self.assertEqual(new_user.first_name, form_data["first_name"])
+		self.assertEqual(new_user.last_name, form_data["last_name"])
+		self.assertEqual(new_user.license_number, form_data["license_number"])
+
+
+class LicenseNumberValidationTests(TestCase):
+	def test_right_license_number(self):
+		license_number = "TES12345"
+		self.assertEqual(
+			license_number,
+			validate_license_number(license_number)
+		)
+
+	def test_license_number_with_wrong_length(self):
+		license_number = "TES1234"
+		license_number_ = "TES1234567"
+		error_message = "License number should consist of 8 characters"
+		with self.assertRaisesMessage(ValidationError, error_message):
+			validate_license_number(license_number)
+			validate_license_number(license_number_)
+
+	def test_first_3_uppercase(self):
+		license_number = "tes12345"
+		license_number_ = "12345678"
+		error_message = "First 3 characters should be uppercase letters"
+		with self.assertRaisesMessage(ValidationError, error_message):
+			validate_license_number(license_number)
+			validate_license_number(license_number_)
+
+	def test_last_5_are_digits(self):
+		license_number = "TEST1234"
+		license_number_ = "TESTTSET"
+		error_message = "Last 5 characters should be digits"
+		with self.assertRaisesMessage(ValidationError, error_message):
+			validate_license_number(license_number)
+			validate_license_number(license_number_)
---
+++ taxi/tests/test_search_forms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.models import Driver, Manufacturer, Car
+
+
+class TestSearch(TestCase):
+	def setUp(self) -> None:
+		self.user = get_user_model().objects.create_user(
+			username="test_user",
+			license_number="TES12345"
+		)
+		self.client.force_login(self.user)
+	
+	def test_search_manufacturer_by_name(self):
+		response = self.client.get(
+			reverse("taxi:manufacturer-list") + "?name=Ford"
+		)
+		self.assertEqual(
+			list(response.context["manufacturer_list"]),
+			list(Manufacturer.objects.filter(name__icontains="Ford")),
+		)
+	
+	def test_search_drivers_by_username(self):
+		response = self.client.get(
+			reverse("taxi:driver-list") + "?name=test_user"
+		)
+		self.assertEqual(
+			list(response.context["driver_list"]),
+			list(Driver.objects.filter(username__icontains="test_user")),
+		)
+	
+	def test_search_cars_by_model(self):
+		response = self.client.get(reverse("taxi:car-list") + "?name=Continental")
+		self.assertEqual(
+			list(response.context["car_list"]),
+			list(Car.objects.filter(model__icontains="Continental")),
+		)
