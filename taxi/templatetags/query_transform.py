ccd32e82a8a60e88a7e15b060a4db07fea52d2ed
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-07 14:56:04 +0200

    Solution2

--- taxi/templatetags/query_transform.py
+++ taxi/templatetags/query_transform.py
@@ -4,12 +4,12 @@ register = template.Library()
 
 
 @register.simple_tag
-def query_transform(request, **kwargs):
+def query_transform(request, **kwargs) -> str:
     updated = request.GET.copy()
+
     for key, value in kwargs.items():
         if value is not None:
             updated[key] = value
         else:
             updated.pop(key, 0)
-
     return updated.urlencode()
