79d950a9c72c17f09db61e9ee4e4eed9250f6be2
Bohdan Kovalenko <bog7807@gmail.com>
2023-03-29 01:51:28 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
+from .forms import ManufacturerSearchForm, CarSearchForm, DriverSearchForm
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm, ManufacturerSearchForm, CarSearchForm, \
-    DriverSearchForm
 
 
 @login_required
