ea5b5c75cecbad6b7143e0942feef695d977f8b3
Maksym Nikityuk <maksym71417@gmail.com>
2023-03-28 14:57:45 +0300

    Tests Solution

---
+++ taxi/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Driver, Car
+
+
+class TestModels(TestCase):
+    def test_manufacturer_str(self) -> None:
+        manu = Manufacturer.objects.create(name="Citroen",
+                                           country="France")
+        self.assertEqual(str(manu), f"{manu.name} {manu.country}")
+
+    def test_driver_str(self) -> None:
+        driver = Driver.objects.create(username="testi",
+                                       password="pastest",
+                                       first_name="Test",
+                                       last_name="Testovich")
+        self.assertEqual(str(driver), f"{driver.username}"
+                                      f" ({driver.first_name}"
+                                      f" {driver.last_name})")
+
+    def test_car_str(self) -> None:
+        manu = Manufacturer.objects.create(name="Citroen",
+                                           country="France")
+        car = Car.objects.create(model="C4", manufacturer=manu)
+
+        self.assertEqual(str(car), car.model)
+
+    def test_create_driver_with_license(self) -> None:
+        username = "Testill"
+        password = "pastest"
+        license_num = "ATB98765"
+        user = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            license_number=license_num
+        )
+
+        self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
+        self.assertEqual(user.license_number, license_num)
--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -6,7 +7,12 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
+from .forms import (DriverCreationForm,
+                    DriverLicenseUpdateForm,
+                    CarForm,
+                    DriverSearchForm,
+                    CarSearchForm,
+                    ManufacturerSearchForm)
 
 
 @login_required
@@ -36,6 +42,24 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ManufacturerListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = ManufacturerSearchForm(initial={
+            "name": name
+        })
+
+        return context
+
+    def get_queryset(self):
+        form = ManufacturerSearchForm(self.request.GET)
+        if form.is_valid():
+            return Manufacturer.objects.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
@@ -58,6 +82,24 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.all().select_related("manufacturer")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CarListView, self).get_context_data(**kwargs)
+
+        model = self.request.GET.get("model", "")
+
+        context["search_form"] = CarSearchForm(initial={
+            "model": model
+        })
+
+        return context
+
+    def get_queryset(self):
+        form = CarSearchForm(self.request.GET)
+        if form.is_valid():
+            return Car.objects.filter(
+                model__icontains=form.cleaned_data["model"]
+            )
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
@@ -84,6 +126,24 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DriverListView, self).get_context_data(**kwargs)
+
+        username = self.request.GET.get("username", "")
+
+        context["search_form"] = DriverSearchForm(initial={
+            "username": username
+        })
+
+        return context
+
+    def get_queryset(self):
+        form = DriverSearchForm(self.request.GET)
+        if form.is_valid():
+            return Driver.objects.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
