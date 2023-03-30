b772e347eebf8944e854f43fcc5b92e961e1a9b3
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-30 19:03:10 +0300

    add test with all cases for license_number validation

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -4,8 +4,9 @@ from taxi.forms import DriverCreationForm
 
 
 class FormsTest(TestCase):
-    def test_driver_creation_form_with_license_number_is_valid(self):
-        form_data = {
+
+    def setUp(self):
+        self.form_data = {
             "username": "new_user",
             "password1": "pass12345",
             "password2": "pass12345",
@@ -13,6 +14,24 @@ class FormsTest(TestCase):
             "last_name": "Test Last name",
             "license_number": "ABC12345",
         }
-        form = DriverCreationForm(data=form_data)
+
+    def test_driver_creation_form_with_license_number_is_valid(self):
+
+        form = DriverCreationForm(data=self.form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data, form_data)
+        self.assertEqual(form.cleaned_data, self.form_data)
+
+    def test_driver_creation_form_with_license_number_is_invalid(self):
+        license_numbers_test_cases = [
+            "abc12345",
+            "12345678",
+            "ABC1234",
+            "ABCD1234",
+            "ABC12345678",
+            "ABC1234!",
+            " "
+        ]
+        for license_number in license_numbers_test_cases:
+            self.form_data["license_number"] = license_number
+            form = DriverCreationForm(data=self.form_data)
+            self.assertFalse(form.is_valid())
