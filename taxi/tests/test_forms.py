30d73c363e325fc4e44623d1416e0a43d77d8306
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-30 18:14:23 +0300

    Add test for all invalid licence number cases

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -16,3 +16,18 @@ class FormsTests(TestCase):
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form.data)
+
+    def test_driver_creation_form_with_invalid_license_number(self):
+        invalid_licence_number = ["TS123", "tsa12345", "12345678", "tsa1234Q"]
+        for number in invalid_licence_number:
+            form_data = {
+                "username": "new_user",
+                "password1": "user123test",
+                "password2": "user123test",
+                "first_name": "Test_First",
+                "last_name": "Test_Last",
+                "license_number": number,
+            }
+            form = DriverCreationForm(data=form_data)
+            self.assertFalse(form.is_valid())
+            self.assertNotEqual(form.cleaned_data, form.data)
