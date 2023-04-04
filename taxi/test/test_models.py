349b43317a0cb47bf0c0c7ce058f78c3d99aac51
Eugene <eugene.lot77@gmail.com>
2023-04-04 17:01:32 +0300

    Made test for craft features (str)

--- taxi/test/test_models.py
+++ taxi/test/test_models.py
@@ -1,59 +1,53 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
 
-from taxi.forms import DriverCreationForm
 from taxi.models import Manufacturer, Car
 
 
-class TestCreateAndDeleteModels(TestCase):
-    def setUp(self):
-        user = get_user_model().objects.create_user(
-            username="tset",
-            password="test12345",
-        )
-
-        self.client.force_login(user)
-
-    def test_form_should_create_driver(self):
-        data = {
-            "username": "test",
-            "license_number": "ABC12345",
-            "password1": "user12345",
-            "password2": "user12345",
-            "first_name": "",
-            "last_name": "",
-        }
-
-        form = DriverCreationForm(data)
-
-        self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data, data)
-
-    def test_should_create_manufacturer(self):
-        before_post = len(Manufacturer.objects.all())
-
-        self.client.post(
-            reverse("taxi:manufacturer-create"),
-            {"name": "test-name", "country": "test-country"},
-        )
-
-        after_post = len(Manufacturer.objects.all())
-
-        self.assertNotEqual(before_post, after_post)
-
-    def test_should_create_car(self):
+class TestModels(TestCase):
+    def test_manufacturer_str(self):
         manufacturer = Manufacturer.objects.create(
-            name="test",
-            country="test_country"
-        )
-        before_post = Car.objects.all()
-
-        self.client.post(
-            reverse("taxi:manufacturer-create"),
-            {"model": "test-model", "manufacturer": manufacturer},
+            name="Test",
+            country="Country"
         )
 
-        after_post = Car.objects.all()
+        self.assertEqual(str(manufacturer), f"Test Country")
 
-        self.assertNotEqual(before_post, after_post)
+    def test_driver_str(self):
+        driver = get_user_model().objects.create_user(
+            username="Test",
+            password="test1234",
+            first_name="Test First",
+            last_name="Test Last"
+        )
+
+        self.assertEqual(
+            str(driver),
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="Test",
+            country="Country"
+        )
+        car = Car.objects.create(
+            model="Test",
+            manufacturer=manufacturer,
+        )
+
+        self.assertEqual(str(car), f"{car.model}")
+
+    def test_create_driver_with_license_number(self):
+        username = "Test"
+        password = "test1234"
+        license_number = "ABC12345"
+        driver = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            license_number=license_number
+        )
+
+        self.assertEqual(driver.username, username)
+        self.assertTrue(driver.check_password(password))
+        self.assertEqual(driver.license_number, license_number)
