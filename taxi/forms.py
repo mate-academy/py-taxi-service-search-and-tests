2f8571b637e03c4d6f11df951bb6ed44a98de5f7
Roman28101 <r.kovalenko28101@gmail.com>
2023-05-29 20:37:30 +0800

    Solution wrong2

--- taxi/forms.py
+++ taxi/forms.py
@@ -64,16 +64,3 @@ class DriverLicenseUpdateForm(forms.ModelForm):
 
     def clean_license_number(self):
         return validate_license_number(self.cleaned_data["license_number"])
-
-
-def validate_license_number(
-    license_number,
-):  # regex validation is also possible here
-    if len(license_number) != 8:
-        raise ValidationError("License number should consist of 8 characters")
-    elif not license_number[:3].isupper() or not license_number[:3].isalpha():
-        raise ValidationError("First 3 characters should be uppercase letters")
-    elif not license_number[3:].isdigit():
-        raise ValidationError("Last 5 characters should be digits")
-
-    return license_number
