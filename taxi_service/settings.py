5e01f3c475247feb9169a87545d533e0202b4fa8
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-06-02 18:16:18 +0300

    'Solution'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "crispy_bootstrap4",
     "crispy_forms",
+    "crispy_bootstrap4",
     "taxi",
 ]
 
