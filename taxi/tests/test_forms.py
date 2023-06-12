557baa780ea28bf869ae22ffee2c1f71b5a25a3d
Dmytry Bondariev <bondarievmd@gmail.com>
2023-06-12 22:03:01 +0300

    Solution3

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.forms import DriverCreationForm, DriverSearchForm
+from taxi.forms import DriverCreationForm
 
 
 class FormsTests(TestCase):
