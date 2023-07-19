4f4a2179cbd4cc93e482d7c17a7650b231c9fbbf
Tarasidze <dtar84@ukr.net>
2023-07-19 23:04:32 +0300

    Solution 0.1 beta :)

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from taxi.models import Manufacturer
+
+
+class ModelsTests(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="Tavria",
+            country="Ukraine"
+        )
+        self.assertEquals(
+            str(manufacturer),
+            f"{manufacturer.name} {manufacturer.country}"
+        )
