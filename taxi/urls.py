c9de56d9cb0270274ddba2293c53c49ccbe630d8
Illya Svyshcho <iill44iikk@gmail.com>
2023-07-22 02:31:59 +0300

    Added: search fields and tests

--- taxi/urls.py
+++ taxi/urls.py
@@ -55,10 +55,10 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/<int:pk>/license-update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
         "drivers/<int:pk>/update/",
