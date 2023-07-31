747c9e2a7a0dca59fa405826a051de4c2971d9cd
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-31 19:24:17 +0300

    Fix flake8

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path("", include("taxi.urls", namespace="taxi")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
