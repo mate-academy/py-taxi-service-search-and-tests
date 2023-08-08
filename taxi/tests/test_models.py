9eda907aa8e7fcbd225782ed4dac331d0c31544e
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-08-08 16:06:38 +0200

    'Solution'

---
+++ taxi/tests/test_models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Car
+
+
+class ModelTests(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="test_name",
+            country="test_country",
+        )
+        self.assertEqual(
+            str(manufacturer),
+            f"{manufacturer.name} {manufacturer.country}"
+        )
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="test_name",
+            country="test_country",
+        )
+        car = Car.objects.create(
+            model="test",
+            manufacturer=manufacturer
+        )
+
+        self.assertEqual(str(car), car.model)
+
+    def test_driver_str(self):
+        driver = get_user_model().objects.create_user(
+            username="test",
+            password="testtest",
+            first_name="first",
+            last_name="last"
+        )
+
+        self.assertEqual(
+            str(driver),
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+
+    def test_create_driver_with_license(self):
+        username = "test"
+        password = "testtest"
+        license_number = "AMD12345"
+        driver = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            license_number=license_number
+        )
+
+        self.assertEqual(driver.username, username)
+        self.assertTrue(driver.check_password(password))
+        self.assertEqual(driver.license_number, license_number)
