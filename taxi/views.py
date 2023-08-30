dfbf574c17bb4f85a75485faefeb66aafb207370
niksgawrilenko <90038040+niksgawrilenko@users.noreply.github.com>
2023-08-30 03:31:45 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -20,17 +20,21 @@ from .forms import (
 @login_required
 def index(request):
     """View function for the home page of the site."""
+
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
+
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
         "num_visits": num_visits + 1,
     }
+
     return render(request, "taxi/index.html", context=context)
 
 
