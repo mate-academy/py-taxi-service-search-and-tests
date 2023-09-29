a46e0239e96c3c126c6eee63c93ccadecb1d90e9
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-09-29 12:30:02 +0300

    Implemented tests for taxi service

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
+
+
+class DriverCreationFormTests(TestCase):
+    def setUp(self) -> None:
+        self.form_data = {
+            "username": "testuser",
+            "password1": "testpassword123",
+            "password2": "testpassword123",
+            "license_number": "ABC12345",
+            "first_name": "Test",
+            "last_name": "User",
+        }
+
+    def test_driver_creation_form_with_valid_data(self):
+        form = DriverCreationForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, self.form_data)
+
+    def test_driver_creation_form_with_invalid_license_number(self):
+        self.form_data["license_number"] = "12AB"
+        form = DriverCreationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+
+class DriverLicenseUpdateFormTests(TestCase):
+    def test_driver_license_update_form_with_valid_data(self):
+        form = DriverLicenseUpdateForm({"license_number": "ABC12345"})
+        self.assertTrue(form.is_valid())
+
+    def test_driver_license_update_form_with_invalid_license_number(self):
+        form = DriverLicenseUpdateForm({"license_number": "12AB"})
+        self.assertFalse(form.is_valid())
