76cc79263378635c42248baf5fac516bc8a5e521
Davydova Olha <o.chookity@gmail.com>
2023-10-02 21:03:56 +0300

    Implement `taxi-service` search and tests

---
+++ taxi/tests/test_admin.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="testadmin"
+        )
+
+        self.client.force_login(self.admin_user)
+
+        self.driver = get_user_model().objects.create_user(
+            username="driver",
+            password="testdriver",
+            license_number="TST12345",
+        )
+
+    def test_driver_license_number_listed(self):
+        url = reverse("admin:taxi_driver_changelist")
+        response = self.client.get(url)
+        self.assertContains(response, self.driver.license_number)
+
+    def test_driver_detail_license_number_listed(self):
+        url = reverse("admin:taxi_driver_change", args=[self.driver.id])
+        response = self.client.get(url)
+        self.assertContains(response, self.driver.license_number)
