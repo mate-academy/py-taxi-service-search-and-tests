b382863921adda6cf66efe902b1dab1f1c8f1575
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-07 00:37:20 +0300

    create TestDriverCreationForm where validate_license_number function is tested

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.forms import DriverUsernameSearchForm
+from taxi.forms import DriverUsernameSearchForm, DriverCreationForm
 
 
 class TestDriverUsernameSearchForm(TestCase):
@@ -35,3 +35,69 @@ class TestDriverUsernameSearchForm(TestCase):
         response = self.client.get(url, data=form_data)
         self.assertContains(response, form_data["username"])
         self.assertNotContains(response, "johnson")
+
+
+class TestDriverCreationForm(TestCase):
+    def test_driver_creation_form_with_valid_license_number(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+            "license_number": "JON26231",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_driver_creation_form_without_license_number(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_driver_creation_form_license_number_too_short(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+            "license_number": "JON2623",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_driver_creation_form_license_number_too_long(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+            "license_number": "JON262334",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_driver_creation_form_license_number_with_only_digits(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+            "license_number": "12345678",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
+
+    def test_driver_creation_form_license_number_with_only_letters(self):
+        form_data = {
+            "username": "admin",
+            "password1": "admin123456",
+            "password2": "admin123456",
+            "license_number": "ABCDEFGHI",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("license_number", form.errors)
