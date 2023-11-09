6848e7cb04e8c603c52e7dcf31258a482ef6a8d5
Artem <hartem472@gmail.com>
2023-11-09 22:52:02 +0100

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from taxi.models import Manufacturer, Driver, Car
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="test_name",
+            country="test_country"
+        )
+        self.assertEqual(
+            str(manufacturer),
+            f"{manufacturer.name} {manufacturer.country}"
+        )
+
+    def test_driver_str(self):
+        driver = Driver.objects.create_user(
+            username="test_username",
+            first_name="jo",
+            last_name="ja"
+        )
+        self.assertEqual(
+            str(driver),
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="test_name",
+            country="test_country"
+        )
+        car = Car.objects.create(model="test_model", manufacturer=manufacturer)
+        self.assertEqual(str(car), car.model)
