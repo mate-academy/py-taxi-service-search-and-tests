26565bfc9284373705600578b34518e1a104675a
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-11-21 23:47:38 +0100

    sol

--- taxi/forms.py
+++ taxi/forms.py
@@ -50,3 +50,15 @@ def validate_license_number(
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+
+class CarSearchByModel(forms.Form):
+    model = forms.CharField(max_length=255, required=False)
+
+
+class DriverSearchByUsername(forms.Form):
+    username = forms.CharField(max_length=255, required=False)
+
+
+class ManufacturerSearchByName(forms.Form):
+    name = forms.CharField(max_length=255, required=False)
