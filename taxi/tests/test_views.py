5ad5420039d20b5b40e626aae1d8d63486196dc0
Roman Zhadan <21gehassig@gmail.com>
2023-11-25 22:02:23 +0200

    add search field to all pages and makes test for views

---
+++ taxi/tests/test_views.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.models import Manufacturer, Driver
+
+
+class TaxiIndexViewTest(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse("taxi:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'taxi/index.html')
+
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.user = Driver.objects.create_user(username="testuser", password="12345")
+
+    def test_manufacturer_list_view(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("taxi:manufacturer-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/manufacturer_list.html")
+
+    def test_car_list_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse("taxi:car-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/car_list.html")
+
+    def test_driver_list_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse("taxi:driver-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/driver_list.html")
