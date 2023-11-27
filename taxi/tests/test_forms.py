0ff564fa5c931caf228649f67db9fbc1f6c16019
Olexandra Ivanova <alexandra.ivva@gmail.com>
2023-11-27 16:25:59 +0200

    Update test_forms.py

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -11,17 +11,17 @@ class FormTests(TestCase):
         ("aaaaaaaa", False),
         ("AAA11111", True),
     ])
-    def test_license_number_is_valid(self):
-        for index, license_number in enumerate(license_numbers):
-            form = DriverCreationForm(
-                data={
-                    "username": "driver",
-                    "password1": "NotCommonPassword123",
-                    "password2": "NotCommonPassword123",
-                    "first_name": "TestName",
-                    "last_name": "Last_name",
-                    "license_number": license_number
-                }
-            )
-            print(form.is_valid())
-            self.assertEqual(form.is_valid(), result[index])
+    def test_license_number_is_valid(self, license_number, expected_result):
+        form_data = {
+            "username": "driver",
+            "password1": "NotCommonPassword123",
+            "password2": "NotCommonPassword123",
+            "first_name": "TestName",
+            "last_name": "Last_name",
+            "license_number": license_number,
+        }
+
+        form = DriverCreationForm(data=form_data)
+        print(form.is_valid())
+        assert form.is_valid() == expected_result
+        
