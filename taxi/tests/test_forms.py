f6cf983d5c4490597e17e7c395446582b890769a
sakhaline <alexandra.ivva@gmail.com>
2023-11-27 23:46:34 +0200

    Install pytest

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,25 +1,27 @@
+import pytest
 from django.test import TestCase
 
 from taxi.forms import DriverCreationForm
 
 
 class FormTests(TestCase):
-    def test_license_number_is_valid(self):
-        license_numbers = [
-            "111", "aaa", "aaa11111", "aaaaaaaa", "AAA11111",
-        ]
-        result = [False, False, False, False, True]
+    @pytest.mark.parametrize("license_number, result", [
+        ("111", False),
+        ("aaa", False),
+        ("aaa11111", False),
+        ("aaaaaaaa", False),
+        ("AAA11111", True),
+    ])
+    def test_license_number_is_valid(self, license_number, result):
+        form_data = {
+            "username": "driver",
+            "password1": "NotCommonPassword123",
+            "password2": "NotCommonPassword123",
+            "first_name": "TestName",
+            "last_name": "Last_name",
+            "license_number": license_number,
+        }
 
-        for index, license_number in enumerate(license_numbers):
-            form = DriverCreationForm(
-                data={
-                    "username": "driver",
-                    "password1": "NotCommonPassword123",
-                    "password2": "NotCommonPassword123",
-                    "first_name": "TestName",
-                    "last_name": "Last_name",
-                    "license_number": license_number
-                }
-            )
-            print(form.is_valid())
-            self.assertEqual(form.is_valid(), result[index])
+        form = DriverCreationForm(data=form_data)
+        print(form.is_valid())
+        assert form.is_valid() == result
