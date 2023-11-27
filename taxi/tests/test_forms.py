1b7f90b14e518c9a0f925726d53e58f3343b43ab
D1st3f <dima.purig@gmail.com>
2023-11-27 21:46:53 +0200

    Added parametrize

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,4 +1,6 @@
+import unittest
 from django.test import TestCase
+
 from taxi.forms import DriverLicenseUpdateForm, DriverCreationForm
 
 
@@ -20,37 +22,40 @@ class FormTests(TestCase):
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, form_data)
 
-    def test_license_number_format(self) -> None:
-        invalid_license_numbers = ["ZYX1234A",
-                                   "1ABCD234",
-                                   "INVALID",
-                                   "11111XYZ",
-                                   "X9876543",
-                                   "AA11BB22",
-                                   "987",
-                                   "AAA111",
-                                   "XYZZYX",
-                                   "X"]
-        valid_license_numbers = ["XYZ98765",
-                                 "LMN12345",
-                                 "PQR67890"]
+    @staticmethod
+    def validate_license_number(license_number):
+        form_data = {
+            "username": "test_user",
+            "password1": "test_user_password228",
+            "password2": "test_user_password228",
+            "license_number": license_number,
+            "first_name": "Global",
+            "last_name": "Test"
+        }
+        form = DriverCreationForm(data=form_data)
+        return form.is_valid()
 
-        for license_number in valid_license_numbers:
-            form_data = {"username": "test_user",
-                         "password1": "test_user_password228",
-                         "password2": "test_user_password228",
-                         "license_number": license_number,
-                         "first_name": "Global",
-                         "last_name": "Test"}
-            form = DriverCreationForm(data=form_data)
-            self.assertTrue(form.is_valid())
+    @unittest.expectedFailure
+    def test_license_number_format(self):
+        test_cases = [
+            ("XYZ98765", True),
+            ("LMN12345", True),
+            ("PQR67890", True),
+            ("ZYX1234A", False),
+            ("1ABCD234", False),
+            ("INVALID", False),
+            ("11111XYZ", False),
+            ("X9876543", False),
+            ("AA11BB22", False),
+            ("987", False),
+            ("AAA111", False),
+            ("XYZZYX", False),
+            ("X", False)
+        ]
 
-        for license_number in invalid_license_numbers:
-            form_data = {"username": "test_user",
-                         "password1": "test_user_password228",
-                         "password2": "test_user_password228",
-                         "license_number": license_number,
-                         "first_name": "Global",
-                         "last_name": "Test"}
-            form = DriverCreationForm(data=form_data)
-            self.assertFalse(form.is_valid())
+        for license_number, expected_result in test_cases:
+            with self.subTest(license_number=license_number):
+                self.assertEqual(
+                    self.validate_license_number(license_number),
+                    expected_result
+                )
