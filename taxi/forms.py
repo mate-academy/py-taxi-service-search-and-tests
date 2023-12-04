1424df3ecaa16744ee523bb29d69b45b565a3733
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-12-04 21:58:47 +0200

    solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-
 from taxi.models import Car, Driver
 
 
@@ -26,7 +25,7 @@ class DriverCreationForm(UserCreationForm):
             "last_name",
         )
 
-    def clean_license_number(self):  # this logic is optional, but possible
+    def clean_license_number(self) -> str:  # this logic is optional, but possible
         return validate_license_number(self.cleaned_data["license_number"])
 
 
@@ -35,18 +34,50 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         model = Driver
         fields = ["license_number"]
 
-    def clean_license_number(self):
+    def clean_license_number(self) -> str:
         return validate_license_number(self.cleaned_data["license_number"])
 
 
 def validate_license_number(
     license_number,
-):  # regex validation is also possible here
+) -> str:  # regex validation is also possible here
     if len(license_number) != 8:
-        raise ValidationError("License number should consist of 8 characters")
-    elif not license_number[:3].isupper() or not license_number[:3].isalpha():
-        raise ValidationError("First 3 characters should be uppercase letters")
+        raise ValidationError(
+            "License number should consist of 8 characters"
+        )
+    elif (not license_number[:3].isupper()
+          or not license_number[:3].isalpha()):
+        raise ValidationError(
+            "First 3 characters should be uppercase letters"
+        )
     elif not license_number[3:].isdigit():
         raise ValidationError("Last 5 characters should be digits")
 
     return license_number
+
+
+class DriversSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"})
+    )
+
+
+class CarsSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by model"})
+    )
+
+
+class ManufacturesSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
