e37a1025cae627907ee196f2adab2d43749f89c9
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-12-28 14:59:39 +0200

    Solution

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
+
+
+class FormsTests(TestCase):
+    def test_driver_creation_form_with_license_number_is_valid(
+        self,
+    ):
+        form_data = {
+            "username": "usertest",
+            "password1": "1q2w3eZXC",
+            "password2": "1q2w3eZXC",
+            "first_name": "test_name",
+            "last_name": "test_lastname",
+            "license_number": "ABC12345",
+        }
+        form = DriverCreationForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+    def test_invalid_license_number_format(self):
+        invalid_license_number = "12345ABC"
+        form_data = {"license_number": invalid_license_number}
+        form = DriverLicenseUpdateForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_valid_license_number(self):
+        valid_license_number = "ABC12345"
+        form_data = {"license_number": valid_license_number}
+        form = DriverLicenseUpdateForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data["license_number"], valid_license_number
+        )
+
+    def test_invalid_license_number_length(self):
+        invalid_license_number = "ABC123"
+        form_data = {"license_number": invalid_license_number}
+        form = DriverLicenseUpdateForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
---
+++ taxi/tests/test_views.py
@@ -0,0 +1,57 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from taxi.forms import DriversSearchForm, CarsSearchForm
+from taxi.models import Driver, Car, Manufacturer
+
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            username="admintest", password="1q2w3eZXC"
+        )
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_search_driver_by_username(self):
+        Driver.objects.create(username="user1", license_number="ABC12345")
+        Driver.objects.create(username="user2", license_number="ABC54321")
+
+        response = self.client.get(
+            reverse("taxi:driver-list"), {"username": "user2"}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(
+            response.context["search_form"], DriversSearchForm
+        )
+        self.assertQuerysetEqual(
+            response.context["object_list"],
+            Driver.objects.filter(username__icontains="user2"),
+        )
+
+    def test_search_car_by_model(self):
+        manufacturer = Manufacturer.objects.create(name="testmanufacturer")
+        driver = get_user_model().objects.create(
+            username="driver1",
+            password="1q2w3eZXC",
+            first_name="test_firstname",
+            last_name="test_lastname",
+            license_number="ABC12345",
+        )
+        car1 = Car.objects.create(model="car1", manufacturer=manufacturer)
+        car2 = Car.objects.create(model="car2", manufacturer=manufacturer)
+        car1.drivers.set([driver])
+        car2.drivers.set([driver])
+
+        response = self.client.get(
+            reverse("taxi:car-list"), {"model": "car1"}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["search_form"], CarsSearchForm)
+        self.assertQuerysetEqual(
+            response.context["object_list"],
+            Car.objects.filter(model__icontains="car1"),
+        )
