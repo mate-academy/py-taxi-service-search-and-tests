ed4c5dc498b0a110ee88aca62e66393ac03247ad
Your_Name Your_Surname <your@email>
2024-01-16 16:14:01 +0200

    Solution

---
+++ taxi/tests/test_views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.models import Manufacturer
+
+MANUFACTURER_URL = reverse("taxi:manufacturer-list")
+
+
+class PublicManufacturerTest(TestCase):
+    def test_login_required_list_page(self):
+        res = self.client.get(MANUFACTURER_URL)
+        self.assertNotEquals(res.status_code, 200)
+
+
+class PrivateManufacturerTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test123",
+        )
+        self.client.force_login(self.user)
+
+    def test_retrive_manufacturer(self):
+        Manufacturer.objects.create(name="test_name1")
+        Manufacturer.objects.create(name="test_name2")
+        response = self.client.get(MANUFACTURER_URL)
+        self.assertEqual(response.status_code, 200)
+        manufacturer = Manufacturer.objects.all()
+        self.assertEqual(
+            list(response.context["manufacturer_list"]),
+            list(manufacturer),
+        )
+        self.assertTemplateUsed(
+            response,
+            "taxi/manufacturer_list.html"
+        )
