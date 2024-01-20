27f8ac16bad7508e7a4b8a3a29a12ac4b6994f18
Ruslan <rusipbox@gmail.com>
2024-01-20 19:58:55 +0200

    Task(py-taxi-service-search-and-tests)

---
+++ taxi/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Car, Driver
+
+
+class ModelTest(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="TestName",
+            country="TestCountry"
+        )
+        expected_result = f"{manufacturer.name} {manufacturer.country}"
+        self.assertEqual(str(manufacturer), expected_result)
+
+    def test_create_driver_and_str(self):
+        username = "TestUsername"
+        password = "Test123#"
+        license_number = "TestLicenseNumber"
+        driver = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            license_number=license_number
+        )
+        self.assertEqual(driver.username, username)
+        self.assertTrue(driver.check_password(password))
+        self.assertEqual(driver.license_number, license_number)
+
+        expected_result = (
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+        self.assertEqual(str(driver), expected_result)
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="TestName",
+            country="Country"
+        )
+        car = Car.objects.create(
+            model="TestModel",
+            manufacturer=manufacturer,
+        )
+
+        expected_result = car.model
+        self.assertEqual(str(car), expected_result)
