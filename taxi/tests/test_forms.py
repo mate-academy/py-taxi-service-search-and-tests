fb223292be7aa995cd46d64c0d70f596c07f42fc
Maxim V <m4k51m15k4m@gmail.com>
2024-01-29 20:14:15 +0200

    add test_models.py

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -10,16 +10,17 @@ DRIVER_LIST_URL = reverse('taxi:driver-list')
 class DriverListViewTest(TestCase):
     def setUp(self):
         # Create a test user
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        user = get_user_model()
+        self.user = user.objects.create(username='test_user', password='test_password')
 
-        # Create some test drivers
-        self.driver1 = Driver.objects.create(username='driver1')
-        self.driver2 = Driver.objects.create(username='driver2')
-        self.driver3 = Driver.objects.create(username='driver3')
+        # Create some test drivers with unique license numbers
+        self.driver1 = Driver.objects.create(username='driver1', license_number='license1')
+        self.driver2 = Driver.objects.create(username='driver2', license_number='license2')
+        self.driver3 = Driver.objects.create(username='driver3', license_number='license3')
 
     def test_search_view_with_results(self):
         # Login the test user
-        self.client.login(username='testuser', password='testpassword')
+        self.client.login(username='test_user', password='test_password')
 
         # Make a GET request with a search query
         response = self.client.get(DRIVER_LIST_URL, {'username': 'driver2'})
@@ -40,10 +41,10 @@ class DriverListViewTest(TestCase):
 
     def test_search_view_with_no_results(self):
         # Login the test user
-        self.client.login(username='testuser', password='testpassword')
+        self.client.login(username='test_user', password='test_password')
 
         # Make a GET request with a search query
-        response = self.client.get(DRIVER_LIST_URL, {'username': 'nonexistentdriver'})
+        response = self.client.get(DRIVER_LIST_URL, {'username': 'non_existent_driver'})
 
         # Check that the response is successful
         self.assertEqual(response.status_code, 200)
@@ -52,7 +53,7 @@ class DriverListViewTest(TestCase):
         self.assertIn('search_form', response.context)
 
         # Check that the search form has the correct initial value
-        self.assertEqual(response.context['search_form'].initial['username'], 'nonexistentdriver')
+        self.assertEqual(response.context['search_form'].initial['username'], 'non_existent_driver')
 
         # Check that there are no matching drivers in the object list
         self.assertNotContains(response, 'driver1')
