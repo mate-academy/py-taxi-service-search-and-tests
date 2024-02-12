8f0b64e1caa28fd60ae656d2b02cba5fb5af60d7
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-02-12 19:08:38 +0200

    Solution 3

--- taxi/urls.py
+++ taxi/urls.py
@@ -21,65 +21,25 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path(
-        "manufacturers/create/",
-        ManufacturerCreateView.as_view(),
-        name="manufacturer-create",
-    ),
-    path(
-        "manufacturers/<int:pk>/update/",
-        ManufacturerUpdateView.as_view(),
-        name="manufacturer-update",
-    ),
-    path(
-        "manufacturers/<int:pk>/delete/",
-        ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete",
-    ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path(
-        "cars/<int:pk>/toggle-assign/",
-        toggle_assign_to_car,
-        name="toggle-car-assign",
-    ),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
-    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path(
-        "drivers/<int:pk>/update/",
-        DriverLicenseUpdateView.as_view(),
-        name="driver-update",
-    ),
-    path(
-        "drivers/<int:pk>/delete/",
-        DriverDeleteView.as_view(),
-        name="driver-delete",
-    ),
-    path("drivers/", views.DriverListView.as_view(), name="driver-list"),
-    path("drivers/search/", views.DriverSearchView.as_view(),
-         name="driver-search"),
+    path("", views.index, name="index"),
+    path("manufacturers/", views.ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("manufacturers/create/", views.ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturers/<int:pk>/update/", views.ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturers/<int:pk>/delete/", views.ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path("manufacturers/search/", views.ManufacturerSearchView.as_view(), name="manufacturer-search"),
     path("cars/", views.CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", views.CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", views.CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", views.CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", views.CarDeleteView.as_view(), name="car-delete"),
     path("cars/search/", views.CarSearchView.as_view(), name="car-search"),
-    path("manufacturers/", views.ManufacturerListView.as_view(),
-         name="manufacturer-list"),
-    path("manufacturers/search/", views.ManufacturerSearchView.as_view(),
-         name="manufacturer-search"),
+    path("cars/<int:pk>/toggle-assign/", views.toggle_assign_to_car, name="toggle-car-assign"),
+    path("drivers/", views.DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", views.DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/create/", views.DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/update/", views.DriverLicenseUpdateView.as_view(), name="driver-update"),
+    path("drivers/<int:pk>/delete/", views.DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/search/", views.DriverSearchView.as_view(), name="driver-search"),
 ]
 
 app_name = "taxi"
