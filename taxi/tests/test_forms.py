a970e60ebac7aaea394741d084a9a1185eb8aa9c
Artur Poltser <polcerartur@gmail.com>
2024-02-29 15:43:07 +0200

    Add more tests

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -14,9 +14,7 @@ class TestForms(TestCase):
             username="test",
             password="test123",
         )
-
-    def test_driver_creation_form_with_additional_fields(self):
-        form_data = {
+        self.form_data = {
             "username": "test_user",
             "password1": "Test123@",
             "password2": "Test123@",
@@ -24,9 +22,22 @@ class TestForms(TestCase):
             "last_name": "test_last",
             "license_number": "BOB11111",
         }
-        form = DriverCreationForm(data=form_data)
+
+    def test_driver_creation_form_with_additional_fields(self):
+        form = DriverCreationForm(data=self.form_data)
         self.assertTrue(form.is_valid())
-        self.assertEquals(form.cleaned_data, form_data)
+        self.assertEquals(form.cleaned_data, self.form_data)
+
+    def test_driver_creation_form_with_no_license(self):
+        del self.form_data["license_number"]
+        form = DriverCreationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_driver_creation_form_with_invalid_license(self):
+        for license_number in ["bob11111", "BOB!1111", "BOB111"]:
+            self.form_data["license_number"] = license_number
+            form = DriverCreationForm(data=self.form_data)
+            self.assertFalse(form.is_valid())
 
     def test_manufacturer_search_form(self):
         form = ManufacturerSearchForm(data={"name": "Test"})
