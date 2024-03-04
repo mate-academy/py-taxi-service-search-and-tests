49ce531766e8a9c8731dd2b92e170177057d28e3
Polonnikova Anna <xxxuko@gmail.com>
2024-03-04 17:02:38 +0200

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from taxi.forms import DriverCreationForm
+from taxi.models import Manufacturer, Car, Driver
+
+
+class ModelTest(TestCase):
+    def setUp(self):
+        self.manufacturer = Manufacturer.objects.create(name="Test", country="TestCountry")
+        self.driver = get_user_model().objects.create(
+            username="Username",
+            first_name="FirstName",
+            last_name="LastName",
+            license_number="AAA12345",
+            email="testemail@gmail.com",
+            password="test123",
+        )
+        self.car = Car.objects.create(
+            model="Model",
+            manufacturer=self.manufacturer,
+        )
+        self.car.drivers.add(self.driver)
+
+    def test_manufacturer_str(self):
+        self.assertEqual(
+            str(self.manufacturer),
+            f"{self.manufacturer.name} {self.manufacturer.country}"
+        )
+
+    def test_driver_str(self):
+        self.assertEqual(
+            str(self.driver),
+            f"{self.driver.username} ({self.driver.first_name} {self.driver.last_name})"
+        )
+
+    def test_car_str(self):
+        self.assertEqual(str(self.car), "Model")
+
+
+class AdminTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="admin123"
+        )
+        self.client.force_login(self.admin_user)
+        self.driver = Driver.objects.create_user(
+            username="driver",
+            password="driver123",
+            license_number="AAA12345",
+        )
+
+    def test_driver_license_number_listed(self):
+        url = reverse("admin:taxi_driver_changelist")
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
+
+    def test_driver_detail_license_number_listed(self):
+        url = reverse("admin:taxi_driver_change", args=[self.driver.id])
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
+
+
+class FormTest(TestCase):
+    def test_driver_creation_form_is_valid(self):
+        form_data = {
+            "username": "new_user",
+            "password1": "user123user",
+            "password2": "user123user",
+            "first_name": "Test first",
+            "last_name": "Test last",
+            "license_number": "AAA12345",
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+
+class PublicViewTest(TestCase):
+    def test_index(self):
+        res = self.client.get(reverse("taxi:index"))
+        self.assertNotEqual(res.status_code, 200)
+
+    def test_manufacturer_list(self):
+        res = self.client.get(reverse("taxi:manufacturer-list"))
+        self.assertNotEqual(res.status_code, 200)
+
+    def test_car_list(self):
+        res = self.client.get(reverse("taxi:car-list"))
+        self.assertNotEqual(res.status_code, 200)
+
+    def test_driver_list(self):
+        res = self.client.get(reverse("taxi:driver-list"))
+        self.assertNotEqual(res.status_code, 200)
