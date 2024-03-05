5055c1eeec3227bb13b8fc286609fb94e4e4ca4e
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-05 17:03:30 +0200

    Solution

---
+++ taxi/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class AccessTest(TestCase):
+    def setUp(self):
+        self.urls = [
+            reverse("taxi:manufacturer-list"),
+            reverse("taxi:manufacturer-create"),
+            reverse("taxi:car-list"),
+            reverse("taxi:car-create"),
+            reverse("taxi:car-detail", kwargs={"pk": 1}),
+            reverse("taxi:driver-list"),
+            reverse("taxi:driver-create"),
+            reverse("taxi:driver-detail", kwargs={"pk": 1}),
+        ]
+
+    def test_login_required_mixin(self):
+        for url in self.urls:
+            self.assertNotEqual(
+                self.client.get(url).status_code,
+                200,
+                msg="One of the page aren't login required"
+            )
