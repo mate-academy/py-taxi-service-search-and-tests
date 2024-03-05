d42f94ea0b76905b4473a9f75a7f84ae327140e8
Yevhen <evgeniy.playid@icloud.com>
2024-03-05 23:50:39 +0200

    'Solution'

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
 
 from taxi.forms import DriverCreationForm, ManufacturerSearchForm, CarSearchForm, DriverSearchForm
 
