84f9c3cfd58df145134233a352017fa351505daa
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-05 18:09:13 +0200

    Sol

--- taxi/apps.py
+++ taxi/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class TaxiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "taxi"
-
--- taxi/forms.py
+++ taxi/forms.py
@@ -57,7 +57,7 @@ class DriverSearchForm(forms.Form):
         max_length=255,
         required=False,
         label="",
-        widget=forms.TextInput(attrs={'placeholder': 'Search by username'})
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
     )
 
 
@@ -66,7 +66,7 @@ class CarSearchForm(forms.Form):
         max_length=255,
         required=False,
         label="",
-        widget=forms.TextInput(attrs={'placeholder': 'Search by model'})
+        widget=forms.TextInput(attrs={"placeholder": "Search by model"}),
     )
 
 
@@ -75,5 +75,5 @@ class ManufacturerSearchForm(forms.Form):
         max_length=255,
         required=False,
         label="",
-        widget=forms.TextInput(attrs={'placeholder': 'Search by name'})
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
     )
