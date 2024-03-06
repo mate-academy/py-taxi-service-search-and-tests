06194e521d5a7113d6ddca8a32a198960a57afd9
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-06 07:47:04 +0200

    Fixed some views

--- taxi/views.py
+++ taxi/views.py
@@ -80,21 +80,6 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
 
-    def get_queryset(self):
-        queryset = Car.objects.select_related("manufacturer")
-        form = CarModelSearchForm(self.request.GET)
-        if form.is_valid():
-            return queryset.filter(model__icontains=form.cleaned_data["model"])
-        return queryset
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(CarListView, self).get_context_data(**kwargs)
-        model = self.request.GET.get("model")
-        context["search_form"] = CarModelSearchForm(
-            initial={"model": model}
-        )
-        return context
-
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
